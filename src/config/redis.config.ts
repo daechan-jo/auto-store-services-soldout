81816742b5dbc3368a1f1049b29887f19beb22d9
JoDaeChan <daechan476@gmail.com>
2025-04-18 05:48:34 +0900

    refactor: simplify dotenv configuration and update redisConfig structure

--- src/config/redis.config.ts
+++ src/config/redis.config.ts
@@ -2,20 +2,13 @@ import { RedisModuleOptions } from '@nestjs-modules/ioredis';
 import * as process from 'node:process';
 import * as dotenv from 'dotenv';
 
-if (process.env.NODE_ENV !== 'PROD') {
-  dotenv.config({
-    path: '/Users/daechanjo/codes/project/auto-store/.env',
-  });
-} else {
-  dotenv.config();
-}
+process.env.NODE_ENV !== 'PROD'
+  ? dotenv.config({
+      path: '/Users/daechanjo/codes/project/auto-store/.env',
+    })
+  : dotenv.config();
 
 export const redisConfig: RedisModuleOptions = {
-  type: process.env.ENV === 'prod' ? 'cluster' : 'single',
-  nodes: [
-    {
-      host: process.env.REDIS_HOST || '127.0.0.1',
-      port: +process.env.REDIS_PORT! || 6379,
-    },
-  ],
+  type: 'single',
+  url: process.env.REDIS_URL,
 };
